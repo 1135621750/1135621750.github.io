abadcfc2f8d557c04995fdafc53a13ebba257d01
bai <1135621750@qq.com>
2018-06-16 01:07:28 +0800

    Site updated: 2018-06-16 01:07:27

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: '后来的我们',
+        artist: '五月天',
+        url: 'http://up.mcyt.net/?down/40588.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: "Butter-Fly",
+        artist: '和田光司 ',
+        url: 'http://up.mcyt.net/?down/41579.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '我的梦',
+        artist: '张靓颖',
+        url: 'http://up.mcyt.net/?down/42094.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      },
+      {
+        name: '夜空中最亮的星',
+        artist: '***',
+        url: 'http://up.mcyt.net/?down/31341.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      },
+      {
+        name: '梦灯笼',
+        artist: '***',
+        url: 'http://up.mcyt.net/?down/44448.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
